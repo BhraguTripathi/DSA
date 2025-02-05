92538083974b0cc771fbb153a612f51c6de221d1
BhraguTripathi <bhragu.12324742@lpu.in>
2025-02-05 09:27:41 +0530

    Day 5

---
+++ src/Arrays/LinearSearch.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class LinearSearch {
+    public static int linearSearch(int findNumber, int arr[]){
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==findNumber){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int findNumber=sc.nextInt();
+        System.out.println(linearSearch(findNumber,arr));
+    }
+}
