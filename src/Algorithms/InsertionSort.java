0475532fa7d1f3f0c181399932e8523f564e3a43
BhraguTripathi <bhragu.12324742@lpu.in>
2025-02-12 21:13:45 +0530

    Day 10

---
+++ src/Algorithms/InsertionSort.java
@@ -0,0 +1,33 @@
+package Algorithms;
+
+import java.util.Scanner;
+
+public class InsertionSort {
+    public static void insertionSort(int arr[],int n){
+        for(int i=1;i<n;i++){
+            int curr=arr[i];
+            int prev=i-1;
+            while(prev>=0 && arr[prev]>curr){
+                arr[prev+1]=arr[prev];
+                prev--;
+            }
+            arr[prev+1]=curr;
+        }
+    }
+    public static void printArray(int arr[], int n){
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        insertionSort(arr,n);
+        printArray(arr,n);
+    }
+}
